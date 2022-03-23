90c41d1356063de78f452ebe2a088c87e5da7c1b
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2022-03-23 17:05:57 +0800

    Separe AWS resolver unit and integration tests

--- resolver-aws-ssm/src/integrationTest/java/io/github/jeyjeyemem/externalizedproperties/resolvers/awsssm/AwsSsmResolverIntegrationTests.java
+++ resolver-aws-ssm/src/integrationTest/java/io/github/jeyjeyemem/externalizedproperties/resolvers/awsssm/AwsSsmResolverIntegrationTests.java
@@ -1,12 +1,22 @@
 package io.github.jeyjeyemem.externalizedproperties.resolvers.awsssm;
 
+import com.github.dockerjava.zerodep.shaded.org.apache.hc.core5.function.Supplier;
 import io.github.jeyjeyemem.externalizedproperties.core.ResolverResult;
-import io.github.jeyjeyemem.externalizedproperties.resolvers.awsssm.testentities.StubSsmClient;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Nested;
 import org.junit.jupiter.api.Test;
+import org.testcontainers.containers.localstack.LocalStackContainer;
+import org.testcontainers.containers.localstack.LocalStackContainer.Service;
+import org.testcontainers.junit.jupiter.Container;
+import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
+import software.amazon.awssdk.auth.credentials.AwsBasicCredentials;
+import software.amazon.awssdk.auth.credentials.StaticCredentialsProvider;
+import software.amazon.awssdk.regions.Region;
 import software.amazon.awssdk.services.ssm.SsmClient;
 
+import java.io.IOException;
 import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
@@ -18,7 +28,36 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-public class AwsSsmResolverTests {
+@Testcontainers
+public class AwsSsmResolverIntegrationTests {
+
+    static final DockerImageName LOCALSTACK_IMAGE = DockerImageName.parse(
+        "localstack/localstack:0.14.1"
+    );
+    
+    @Container
+    static final LocalStackContainer LOCALSTACK = 
+        new LocalStackContainer(LOCALSTACK_IMAGE)
+            .withServices(Service.SSM);
+
+    // Should only be called after container has started 
+    // e.g. in @Nested classes or @Test methods.
+    static final Supplier<SsmClient> SSM_CLIENT_FACTORY = () -> SsmClient.builder()
+        .endpointOverride(LOCALSTACK.getEndpointOverride(Service.SSM))
+        .credentialsProvider(StaticCredentialsProvider.create(
+            AwsBasicCredentials.create(
+                LOCALSTACK.getAccessKey(), 
+                LOCALSTACK.getSecretKey()
+            )
+        ))
+        .region(Region.of(LOCALSTACK.getRegion()))
+        .build();
+
+    @BeforeAll
+    static void setup() throws IOException, InterruptedException {
+        createTestSsmParameters();
+    }
+
     @Nested
     class Constructor {
         @Test
@@ -33,11 +72,14 @@ public class AwsSsmResolverTests {
 
     @Nested
     class ResolveMethod {
+
+        private final SsmClient ssmClient = SSM_CLIENT_FACTORY.get();
+            
         @Test
         @DisplayName("should throw when propertyName argument is null")
         void test1() {
             AwsSsmResolver awsSsmPropertyResolver = 
-                new AwsSsmResolver(new StubSsmClient());
+                new AwsSsmResolver(ssmClient);
 
             assertThrows(IllegalArgumentException.class, () -> {
                 awsSsmPropertyResolver.resolve((String)null);
@@ -48,7 +90,7 @@ public class AwsSsmResolverTests {
         @DisplayName("should throw when propertyName argument is empty")
         void test2() {
             AwsSsmResolver awsSsmPropertyResolver = 
-                new AwsSsmResolver(new StubSsmClient());
+                new AwsSsmResolver(ssmClient);
                 
             assertThrows(IllegalArgumentException.class, () -> {
                 awsSsmPropertyResolver.resolve("");
@@ -59,7 +101,7 @@ public class AwsSsmResolverTests {
         @DisplayName("should resolve property from AWS SSM")
         void test3() {
             AwsSsmResolver awsSsmPropertyResolver = 
-                new AwsSsmResolver(new StubSsmClient());
+                new AwsSsmResolver(ssmClient);
 
             Optional<String> resolved = awsSsmPropertyResolver.resolve("/test/property");
 
@@ -71,10 +113,10 @@ public class AwsSsmResolverTests {
         @DisplayName("should return empty Optional when property is not found in AWS SSM")
         void tes4() {
             AwsSsmResolver awsSsmPropertyResolver = 
-                new AwsSsmResolver(StubSsmClient.NO_PARAMETERS);
+                new AwsSsmResolver(ssmClient);
 
             Optional<String> resolved = awsSsmPropertyResolver.resolve(
-                "/non/existing/property"
+                "non/existing/property"
             );
 
             assertFalse(resolved.isPresent());
@@ -84,11 +126,13 @@ public class AwsSsmResolverTests {
     @Nested
     class ResolveMethodWithCollectionOverload {
 
+        private final SsmClient ssmClient = SSM_CLIENT_FACTORY.get();
+            
         @Test
         @DisplayName("should throw when propertyNames argument is null")
         void test1() {
             AwsSsmResolver awsSsmPropertyResolver = 
-                new AwsSsmResolver(new StubSsmClient());
+                new AwsSsmResolver(ssmClient);
 
             assertThrows(IllegalArgumentException.class, () -> {
                 awsSsmPropertyResolver.resolve((Collection<String>)null);
@@ -99,7 +143,7 @@ public class AwsSsmResolverTests {
         @DisplayName("should throw when propertyNames argument is empty")
         void test2() {
             AwsSsmResolver awsSsmPropertyResolver = 
-                new AwsSsmResolver(new StubSsmClient());
+                new AwsSsmResolver(ssmClient);
                 
             assertThrows(IllegalArgumentException.class, () -> {
                 awsSsmPropertyResolver.resolve(Collections.emptyList());
@@ -110,7 +154,7 @@ public class AwsSsmResolverTests {
         @DisplayName("should resolve all properties from AWS SSM")
         void test3() {
             AwsSsmResolver awsSsmPropertyResolver = 
-                new AwsSsmResolver(new StubSsmClient());
+                new AwsSsmResolver(ssmClient);
 
             List<String> propertiesToResolve = Arrays.asList(
                 "/test/property",
@@ -133,7 +177,7 @@ public class AwsSsmResolverTests {
             );
 
             assertEquals(
-                "test.property.2.value", 
+                "test.property.value.2", 
                 result.findRequiredProperty("/test/property/2")
             );
         }
@@ -143,19 +187,13 @@ public class AwsSsmResolverTests {
             "should return result with resolved and unresolved properties from AWS SSM"
         )
         void test4() {
-            // Only returns properties that starts with test/property
-            SsmClient stubSsmClient = new StubSsmClient(
-                p -> p.startsWith("/test/property") ? 
-                    StubSsmClient.DEFAULT_DELEGATE.apply(p) : 
-                    null
-            );
             AwsSsmResolver awsSsmPropertyResolver = 
-                new AwsSsmResolver(stubSsmClient);
+                new AwsSsmResolver(ssmClient);
 
             List<String> propertiesToResolve = Arrays.asList(
-                "/test/property", 
-                "/test/property/2",
-                "/non/existent/property"
+                "test/property", 
+                "test/property/2",
+                "non/existent/property"
             );
 
             ResolverResult result = 
@@ -170,21 +208,24 @@ public class AwsSsmResolverTests {
             );
 
             assertEquals(
-                "test.property.2.value", 
+                "test.property.value.2", 
                 result.findRequiredProperty("/test/property/2")
             );
 
-            assertTrue(result.unresolvedPropertyNames().contains("/non/existent/property"));
+            assertTrue(result.unresolvedPropertyNames().contains("non/existent/property"));
         }
     }
 
     @Nested
     class ResolveMethodWithVarArgsOverload {
+
+        private final SsmClient ssmClient = SSM_CLIENT_FACTORY.get();
+            
         @Test
         @DisplayName("should throw when propertyNames argument is null")
         void test1() {
             AwsSsmResolver awsSsmPropertyResolver = 
-                new AwsSsmResolver(new StubSsmClient());
+                new AwsSsmResolver(ssmClient);
 
             assertThrows(IllegalArgumentException.class, () -> {
                 awsSsmPropertyResolver.resolve((String[])null);
@@ -195,7 +236,7 @@ public class AwsSsmResolverTests {
         @DisplayName("should throw when propertyNames argument is empty")
         void test2() {
             AwsSsmResolver awsSsmPropertyResolver = 
-                new AwsSsmResolver(new StubSsmClient());
+                new AwsSsmResolver(ssmClient);
                 
             assertThrows(IllegalArgumentException.class, () -> {
                 awsSsmPropertyResolver.resolve(new String[0]);
@@ -206,7 +247,7 @@ public class AwsSsmResolverTests {
         @DisplayName("should resolve all properties from AWS SSM")
         void test3() {
             AwsSsmResolver awsSsmPropertyResolver = 
-                new AwsSsmResolver(new StubSsmClient());
+                new AwsSsmResolver(ssmClient);
 
             String[] propertiesToResolve = new String[] {
                 "/test/property",
@@ -229,7 +270,7 @@ public class AwsSsmResolverTests {
             );
 
             assertEquals(
-                "test.property.2.value", 
+                "test.property.value.2", 
                 result.findRequiredProperty("/test/property/2")
             );
         }
@@ -239,19 +280,13 @@ public class AwsSsmResolverTests {
             "should return result with resolved and unresolved properties from AWS SSM"
         )
         void test4() {
-            // Only returns properties that starts with test/property
-            SsmClient stubSsmClient = new StubSsmClient(
-                p -> p.startsWith("/test/property") ? 
-                    StubSsmClient.DEFAULT_DELEGATE.apply(p) : 
-                    null
-            );
             AwsSsmResolver awsSsmPropertyResolver = 
-                new AwsSsmResolver(stubSsmClient);
+                new AwsSsmResolver(ssmClient);
 
             String[] propertiesToResolve = new String[] {
-                "/test/property", 
-                "/test/property/2",
-                "/non/existent/property"
+                "test/property", 
+                "test/property/2",
+                "non/existent/property"
             };
 
             ResolverResult result = 
@@ -266,12 +301,63 @@ public class AwsSsmResolverTests {
             );
 
             assertEquals(
-                "test.property.2.value", 
+                "test.property.value.2", 
                 result.findRequiredProperty("/test/property/2")
             );
 
-            assertTrue(result.unresolvedPropertyNames().contains("/non/existent/property"));
+            assertTrue(result.unresolvedPropertyNames().contains("non/existent/property"));
         }
+    }
+    
+    // Create parameters for testing.
+    static void createTestSsmParameters() throws IOException, InterruptedException {
+        LOCALSTACK.execInContainer(
+            "awslocal",
+            "ssm",
+            "put-parameter",
+            "--name",
+            "/test/property",
+            "--value",
+            "test.property.value",
+            "--type", 
+            "String"
+        );
+
+        LOCALSTACK.execInContainer(
+            "awslocal",
+            "ssm",
+            "put-parameter",
+            "--name",
+            "/test/property/2",
+            "--value",
+            "test.property.2.value",
+            "--type", 
+            "String"
+        );
+
+        LOCALSTACK.execInContainer(
+            "awslocal",
+            "ssm",
+            "put-parameter",
+            "--name",
+            "/test/property/secure",
+            "--value",
+            "test.property.secure.value",
+            "--type", 
+            "SecureString"
+        );
+
+        LOCALSTACK.execInContainer(
+            "awslocal",
+            "ssm",
+            "put-parameter",
+            "--name",
+            "/test/property/list",
+            "--value",
+            "test.property.list.value",
+            "--type", 
+            "StringList"
+        );
     }
 
 }
