d710bf4f27698b9d8921c8c361cdd0ddda509b72
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2022-05-17 17:11:23 +0800

    [skip ci] Javadoc - update resource reader docs

--- core/src/test/java/io/github/joeljeremy7/externalizedproperties/core/resolvers/resourcereaders/JsonReader.java
+++ core/src/test/java/io/github/joeljeremy7/externalizedproperties/core/resolvers/resourcereaders/JsonReader.java
@@ -14,9 +14,10 @@ import java.util.Map;
  * <p>
  * This is a simple implementation which serializes a JSON resource to a {@code Map}.
  * It is not included in the library as a built-in {@link ResourceReader} because 
- * we want to minimize external dependencies as much as possible to keep the library 
- * size small. It is easy enough to implement so I decided to put this here instead to
- * serve as an example for anyone who wants to implement reading of properties from JSON.
+ * I want to minimize external dependencies as much as possible to keep the library 
+ * size small and avoid possible dependency conflicts with client code. 
+ * It is easy enough to implement so I decided to put this here instead to serve as an 
+ * example for anyone who wants to implement reading of properties from JSON.
  * </p>
  */
 public class JsonReader implements ResourceReader {
