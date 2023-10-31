73e7be557abbb9d5861935a19d9d3705a3a0f12c
Jeremy Marquez <jmarquez@starlinewindows.com>
2023-10-31 15:03:08 -0700

    module-info.java + gradle upgrade

--- build-logic/settings.gradle.kts
+++ build-logic/settings.gradle.kts
@@ -1,3 +1,17 @@
+plugins {
+    id("org.gradle.toolchains.foojay-resolver") version "0.7.0"
+}
+
+toolchainManagement {
+  jvm { 
+    javaRepositories {
+      repository("foojay") { 
+        resolverClass.set(org.gradle.toolchains.foojay.FoojayToolchainResolver::class.java)
+      }
+    }
+  }
+}
+
 dependencyResolutionManagement {
   repositories {
     gradlePluginPortal()
---
+++ externalized-properties-git/src/main/java/module-info.java
@@ -0,0 +1,6 @@
+module io.github.joeljeremy.externalizedproperties.git {
+  exports io.github.joeljeremy.externalizedproperties.git;
+
+  requires transitive org.eclipse.jgit;
+  requires transitive io.github.joeljeremy.externalizedproperties.core;
+}
