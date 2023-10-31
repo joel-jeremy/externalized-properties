ce201e13732a72026718bb5e9e43604ca1d36022
Jeremy Marquez <jmarquez@starlinewindows.com>
2023-10-31 08:07:27 -0700

    Move module-info.java

--- resolvers/database/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module io.github.joeljeremy7.externalizedproperties.resolvers.database {
-    exports io.github.joeljeremy7.externalizedproperties.resolvers.database;
-    exports io.github.joeljeremy7.externalizedproperties.resolvers.database.queryexecutors;
-
-    requires transitive io.github.joeljeremy7.externalizedproperties.core;
-    requires transitive java.sql;
-    requires static org.checkerframework.checker.qual;
-}
--- resolvers/git/src/main/java/module-info.java
+++
@@ -1,6 +0,0 @@
-module io.github.joeljeremy7.externalizedproperties.resolvers.git {
-    exports io.github.joeljeremy7.externalizedproperties.resolvers.git;
-
-    requires transitive org.eclipse.jgit;
-    requires transitive io.github.joeljeremy7.externalizedproperties.core;
-}
