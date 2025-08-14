542e0836149cd5eb4505273e87b80b6b8d2882c0
Joel Jeremy Marquez <joeljeremy.marquez@gmail.com>
2025-08-14 13:25:31 -0700

    Add org argument to snyk plugin (#223)

--- build-logic/src/main/kotlin/externalized-properties.snyk-conventions.gradle.kts
+++ build-logic/src/main/kotlin/externalized-properties.snyk-conventions.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 snyk {
   setApi(findProperty("snykToken") as String?)
   setSeverity("low")
-  setArguments("--all-sub-projects --sarif-file-output=snyk.sarif")
+  setArguments("--org=3bd738c1-9bba-45b8-b13d-9870bd4a8a4f --all-sub-projects --sarif-file-output=snyk.sarif")
   setAutoDownload(true)
   setAutoUpdate(true)
 }
